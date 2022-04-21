44099442ecb5702c0078af27d8f9112bdebff1b5
andv <andv@vietis.com.vn>
2022-04-21 09:05:28 +0700

    fix count time from wedding days

--- public/count-down.js
+++ public/count-down.js
@@ -43,18 +43,19 @@
         }, 0)
 }());
 
-//count from 16-01-2022
-var startDateTime = new Date(2022,1,14,0,0,0,0); // YYYY (M-1) D H m s (start time and date from DB)
-var startStamp = startDateTime.getTime();
 
-var newDate = new Date();
-var newStamp = newDate.getTime();
-
-var timer;
 
 function updateClock() {
-    newDate = new Date();
-    newStamp = newDate.getTime();
+    //count from 16-01-2022
+    var startDateTime = new Date(2022,0,14,0,0,0,0); // YYYY (M-1) D H m s (start time and date from DB)
+    var startStamp = startDateTime.getTime();
+
+    var newDate = new Date();
+    var newStamp = newDate.getTime();
+
+    // var timer;
+    // newDate = new Date();
+    // newStamp = newDate.getTime();
     var diff = Math.round((newStamp-startStamp)/1000);
     
     var d = Math.floor(diff/(24*60*60));
